40b51e08e9ac62f4de93298380e314558bf87d50
Rafael Pereira Buçard <rafaelbucard@gmail.com>
2020-10-31 02:23:56 -0300

    DB config add

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$db_host = 'localhost';
+$db_name = 'devsnote';
+$db_user = 'root';
+$db_pass = '';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host",$db_user,$db_pass);
+$array = [
+    'error'=> '',
+    'result'=>[]
+];
